ddca02f29b10bd332d562afa41af76528e28f810
Issac555 <98994506+Issac555@users.noreply.github.com>
2026-04-17 09:49:57 -0500

    Update main.py

--- Ejercicios/Semana #1/main.py
+++ Ejercicios/Semana #1/main.py
@@ -2,16 +2,22 @@ bass = 3
 
 print(type(bass))
 
-def sumar(a, b):
-    return a + b
-resultado = sumar(3, 2)
-print(resultado)
+a = "Isaac"
+f = "Vega"
+e = "19"
+c = "Guayaquil"
+i = "5"
+age = 1
+print("Mi nombre es" + a + "" + f + "tengo " + e + "años" + "Soy de la ciudad de " + c + "y somos " + i + "integrantes de mi familia")
 
+def Elderly(age):
+    if age > 18 and age % 2 == 0: #Detecta si es mayor de edad y es divisible
+        print("Is he/she of legal age and is divisible by two")
+    elif age > 18 and age % 2 != 0: #Detecta si es menor de edad y no es divible
+        print("Is he/she of legal age and is not divisible by two")
+    elif age <= 18 and age % 2 == 0: #Detecta si es menor o igual y si es divisible
+        print("He/she is not of legal age and is divisible by two")
+    else: #Detecta que no es mayor de edad y no es divible
+        print("He/she is not of legal age and is not divisible by two")
 
-def mayorEdad(a):
-    if a < 18:
-        return print("Es menor de edad")
-    else :
-        return print("Es mayor de edad")
-
-mayorEdad(19)
+Elderly(20)

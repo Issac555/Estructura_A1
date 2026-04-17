75ba056f7fe2b6c380651a0dba87edbec7f81c63
Isaac <pjeremy381@gmail.com>
2026-04-17 08:58:04 -0500

    Upload

---
+++ Ejercicios/Semana #1/main.py
@@ -0,0 +1,17 @@
+bass = 3
+
+print(type(bass))
+
+def sumar(a, b):
+    return a + b
+resultado = sumar(3, 2)
+print(resultado)
+
+
+def mayorEdad(a):
+    if a < 18:
+        return print("Es menor de edad")
+    else :
+        return print("Es mayor de edad")
+
+mayorEdad(19)

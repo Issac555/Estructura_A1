e0f82ba410154af822248dba43ec52b42b273433
Isaac <pjeremy381@gmail.com>
2026-04-17 09:17:35 -0500

    Upload

--- Ejercicios/Semana #1/main.py
+++ Ejercicios/Semana #1/main.py
@@ -2,6 +2,15 @@ bass = 3
 
 print(type(bass))
 
+a = "Isaac"
+f = "Vega"
+e = "19"
+c = "Guayaquil"
+i = "5"
+
+print("Mi nombre es" + a + "" + f + "tengo " + e + "años" + "Soy de la ciudad de " + c + "y somos " + i + "integrantes de mi familia")
+
+
 def sumar(a, b):
     return a + b
 resultado = sumar(3, 2)
